fc7ff74cef31589284228b115e36929e609e0867
tonissonm <116629360+tonissonm@users.noreply.github.com>
2024-09-10 10:56:23 +0300

    Ei taha, et minu parooli nähtaks.

--- joga_mysql_oop/utils/db.js
+++ joga_mysql_oop/utils/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2')
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Imanoobganker1!',
+    password: '',
     database: 'joga_mysql'
 })
 
