1703f7fa0ab08efe4dd5846feba518700edb4041
Matis Tonisson <matistonisson@gmail.com>
2024-09-18 13:30:08 +0300

    Created a connection to the database.

--- joga_sequelize/index.js
+++ joga_sequelize/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 
+const { Sequelize } = require('sequelize');
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3026/joga_sequelize');
+sequelize
+    .authenticate()
+    .then(()=>{
+    console.log('Connected to the database.');
+    })
+    .catch(err =>{
+        console.error('Unable to connect to the database: ',err);
+    });
+
+
 app.use(express.json());
 
 app.use(express.urlencoded({extended:true}));
